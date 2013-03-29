db570f897017a0ce6394cce04e812aeecf4f6b81
Abdullah Al Mamun (Oronno) <oronno@therapservices.net>
2013-03-29 12:01:50 +0600

    Added new constructor and introduce new methods
    - Added new constructor what take both class and tag.
    - Handled throwable more natural way, now can pass throwable
    as the last argument.
    - Added new methods similar with android Log.

--- src/main/java/com/oneous/log4android/Logger.java
+++ src/main/java/com/oneous/log4android/Logger.java
@@ -27,14 +27,19 @@ public class Logger {
     private static final String DELIM_STR = "{}";
     private String prefixMsg;
 
-    private Logger(Class c) {
-        this.TAG = getTag(c);
-        this.prefixMsg = c.getName() + " >> ";
+    private Logger(Class clazz) {
+        this.TAG = getTag(clazz);
+        this.prefixMsg = clazz.getName() + " >> ";
     }
 
     private Logger(String tag) {
         this.TAG = tag;
         this.prefixMsg = "";
+    }
+
+    private Logger(Class clazz, String tag) {
+        this.TAG = tag;
+        this.prefixMsg = clazz.getName() + " >> ";
     }
 
     /**
@@ -43,17 +48,17 @@ public class Logger {
      * TAG will be derive from the package name.
      * So, if your package start as com.google.map.... then "google" will use as TAG.
      *
-     * @param c The Class need to log
+     * @param clazz The Class need to log
      * @return Logger implementation object
      */
-    public static Logger getLogger(Class c) {
-        return new Logger(c);
+    public static Logger getLogger(Class clazz) {
+        return new Logger(clazz);
     }
 
     /**
      * Return Logger implementation object.
      * Provided tag string will be used as TAG.
-     * Will not print Fully qualified Class name.
+     * Will not print Fully qualified Class name with Message.
      *
      * @param tag Used to identify the source of a log message.
      * @return Logger implementation object
@@ -62,20 +67,61 @@ public class Logger {
         return new Logger(tag);
     }
 
+    /**
+     * Return Logger implementation object.
+     * Provided tag string will be used as TAG.
+     * Fully qualified Class name will printed as prefix with log message.
+     *
+     * @param clazz The Class need to log
+     * @param tag   Used to identify the source of a log message.
+     * @return Logger implementation object
+     */
+    public static Logger getLogger(Class clazz, String tag) {
+        return new Logger(clazz, tag);
+    }
+
     public void verbose(Object obj) {
         Log.v(TAG, getString(obj));
     }
 
+    /**
+     * Log a message at the VERBOSE level according to the specified format and arguments.
+     * Can pass Throwable instance as last argument. Like
+     * <code>log.verbose("Exception caught, where, var1={}, var2={}", var1, var2, throwable);</code>
+     *
+     * @param message the format string
+     * @param args    the arguments. Throwable can be passed as last argument and will print accordingly.
+     */
     public void verbose(String message, Object... args) {
-        Log.v(TAG, formatMessage(message, args));
+        Throwable tr = getThrowable(args);
+
+        if (tr == null) {
+            Log.v(TAG, formatMessage(message, args));
+        } else {
+            Log.v(TAG, formatMessage(message, args), tr);
+        }
     }
 
     public void debug(Object obj) {
         Log.v(TAG, getString(obj));
     }
 
+    /**
+     * Log a message at the DEBUG level according to the specified format and arguments.
+     * Can pass Throwable instance as last argument. Like
+     * <code>log.debug("Exception caught, where, var1={}, var2={}", var1, var2, throwable);</code>
+     *
+     * @param message the format string
+     * @param args    the arguments. Throwable can be passed as last argument and will print accordingly.
+     */
     public void debug(String message, Object... args) {
-        Log.d(TAG, formatMessage(message, args));
+        Throwable tr = getThrowable(args);
+
+        if (tr == null) {
+            Log.d(TAG, formatMessage(message, args));
+        } else {
+            Log.d(TAG, formatMessage(message, args), tr);
+        }
     }
 
     public void info(Object obj) {
@@ -83,9 +129,16 @@ public class Logger {
     }
 
     public void info(String message, Object... args) {
-        Log.i(TAG, formatMessage(message, args));
+        Throwable tr = getThrowable(args);
+
+        if (tr == null) {
+            Log.i(TAG, formatMessage(message, args));
+        } else {
+            Log.i(TAG, formatMessage(message, args), tr);
+        }
     }
 
+    @Deprecated
     public void info(Throwable e, String message, Object... args) {
         Log.i(TAG, formatMessage(message, args), e);
     }
@@ -94,10 +147,25 @@ public class Logger {
         Log.v(TAG, getString(obj));
     }
 
+    /**
+     * Log a message at the WARN level according to the specified format and arguments.
+     * Can pass Throwable instance as last argument. Like
+     * <code>log.warn("Exception caught, where, var1={}, var2={}", var1, var2, throwable);</code>
+     *
+     * @param message the format string
+     * @param args    the arguments. Throwable can be passed as last argument and will print accordingly.
+     */
     public void warn(String message, Object... args) {
-        Log.w(TAG, formatMessage(message, args));
+        Throwable tr = getThrowable(args);
+
+        if (tr == null) {
+            Log.w(TAG, formatMessage(message, args));
+        } else {
+            Log.w(TAG, formatMessage(message, args), tr);
+        }
     }
 
+    @Deprecated
     public void warn(Throwable e, String message, Object... args) {
         Log.w(TAG, formatMessage(message, args), e);
     }
@@ -106,22 +174,67 @@ public class Logger {
         Log.v(TAG, getString(obj));
     }
 
+    /**
+     * Log a message at the ERROR level according to the specified format and arguments.
+     * Can pass Throwable instance as last argument. Like
+     * <code>log.error("Error Occur, var1={}, var2={}", var1, var2, throwable);</code>
+     *
+     * @param message the format string
+     * @param args    the arguments. Throwable can be passed as last argument and will print accordingly.
+     */
     public void error(String message, Object... args) {
-        Log.e(TAG, formatMessage(message, args));
+        Throwable tr = getThrowable(args);
+
+        if (tr == null) {
+            Log.e(TAG, formatMessage(message, args));
+        } else {
+            Log.e(TAG, formatMessage(message, args), tr);
+        }
     }
 
-    public void error(Throwable e, String message, Object... args) {
-        Log.e(TAG, formatMessage(message, args), e);
+    /**
+     * Log an exception (throwable) at the ERROR level with an
+     * accompanying message.
+     *
+     * @param message the message accompanying the exception
+     * @param tr      the exception (throwable) to log
+     */
+    public void error(String message, Throwable tr) {
+        Log.e(TAG, message, tr);
+    }
+
+    /**
+     * Deprecated. Instead print log with throwable like
+     * <code>log.error("Error Occur", throwable);</code>
+     *
+     * @param tr      the exception (throwable) to log
+     * @param message the message accompanying the exception
+     * @param args    the arguments
+     */
+    @Deprecated
+    public void error(Throwable tr, String message, Object... args) {
+        Log.e(TAG, formatMessage(message, args), tr);
     }
 
     public void wtf(Object obj) {
         Log.v(TAG, getString(obj));
     }
 
+    /**
+     * What a Terrible Failure: Report a condition that should never happen.
+     * The error will always be logged at level ASSERT with the call stack.
+     *
+     * Can pass Throwable instance as last argument. Like
+     * <code>log.wtf("Exception caught, where, var1={}, var2={}", var1, var2, throwable);</code>
+     *
+     * @param message the format string
+     * @param args    the arguments. Throwable can be passed as last argument and will print accordingly.
+     */
     public void wtf(String message, Object... args) {
         Log.wtf(TAG, formatMessage(message, args));
     }
 
+    @Deprecated
     public void wtf(Throwable e, String message, Object... args) {
         Log.wtf(TAG, formatMessage(message, args), e);
     }
@@ -135,6 +248,22 @@ public class Logger {
     public static String getStackTraceString(Throwable tr) {
         return Log.getStackTraceString(tr);
     }
+
+    /**
+     * Checks to see whether or not a log for the specified tag is loggable
+     * at the specified level. The default level of any tag is set to INFO.
+     * This means that any level above and including INFO will be logged.
+     * Before you make any calls to a logging method you should check to see
+     * if your tag should be logged
+     * .
+     * @param tag The tag to check.
+     * @param level The level to check.
+     * @return Whether or not that this is allowed to be logged.
+     */
+    public static boolean isLoggable (String tag, int level) {
+        return Log.isLoggable(tag, level);
+    }
+
 
     private String formatMessage(String message, Object... args) {
 
@@ -172,5 +301,18 @@ public class Logger {
         }
 
         return prefixMsg + obj.toString();
+    }
+
+    private Throwable getThrowable(Object[] args) {
+        if (args == null || args.length == 0) {
+            return null;
+        }
+
+        Object lastObject = args[args.length - 1];
+        if (lastObject instanceof Throwable) {
+            return (Throwable) lastObject;
+        }
+
+        return null;
     }
 }
